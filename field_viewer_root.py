63bf82e844a82329602e335a4afe80e42fba148d
ScottDemarest <38106816+ScottDemarest@users.noreply.github.com>
2023-04-19 11:35:06 -0400

    fast field generation!

--- field_viewer_root.py
+++ field_viewer_root.py
@@ -8,14 +8,17 @@ with uproot.open("B_field_inputs.root") as f:
     By_f = f["B_field"]["By"].array()
     Bz_f = f["B_field"]["Bz"].array()
 
-dim_half = 20
+dim_half = 16
 
 slice_y = By_f[dim_half][:][:]
+#slice_y = Bx_f[:][dim_half][:]
+#slice_y = By_f[:][:]
 shape = np.shape(slice_y)
 print(f"Before saving shape: {shape}")
 
 slice_z = Bz_f[dim_half][:][:]
-
+#slice_z = Bz_f[:][dim_half][:]
+#slice_z = Bz_f[:][:]
 
 @np.vectorize
 def mapping_y(i, j):
@@ -27,12 +30,16 @@ def mapping_z(i, j):
 
 Y, Z = np.mgrid[0:shape[0], 0:shape[1]] 
 # U, V = slice_y[Y][Z], slice_z[Y][Z]
-U = mapping_y(Y.T,Z.T)
-V = mapping_z(Y.T,Z.T)
-print(f"U shape: {np.shape(U)}")
+#U = mapping_y(Y.T,Z.T)
+#V = mapping_z(Y.T,Z.T)
 
+U = mapping_y(Y,Z)
+V = mapping_z(Y,Z)
+
+print(f"U shape: {np.shape(U)}")
+r = np.power(np.add(np.power(U,2), np.power(V,2)),0.5)
 skip = (slice(None, None, 8), slice(None, None, 8))
-plt.quiver(Y, Z, U, V)
+plt.quiver(Y, Z, U/r, V/r, width=.005, pivot='mid')
 plt.savefig("By_Bz.png", bbox_inches="tight", dpi=300, )
 
 # with uproot.recreate("vector_fields.root") as f:
@@ -44,3 +51,37 @@ plt.savefig("By_Bz.png", bbox_inches="tight", dpi=300, )
 #     Bx = f["B_field"]["Bx"].array()
     
 # print(np.shape(Bx))
+
+dim_half = 16
+
+slice_x = Bx_f[:][dim_half][:]
+#slice_y = Bx_f[::][dim_half][:]
+#slice_y = By_f[:][:]
+shape = np.shape(slice_x)
+print(f"Before saving shape: {shape}")
+
+slice_z = Bz_f[:][dim_half][:]
+#slice_z = Bz_f[:][dim_half][:]
+#slice_z = Bz_f[:][:]
+
+@np.vectorize
+def mapping_x(i, j):
+    return slice_x[i][j]
+
+@np.vectorize
+def mapping_z(i, j):
+    return slice_z[i][j]
+
+X, Z = np.mgrid[0:shape[0], 0:shape[1]] 
+# U, V = slice_y[Y][Z], slice_z[Y][Z]
+#U = mapping_y(Y.T,Z.T)
+#V = mapping_z(Y.T,Z.T)
+
+U = mapping_y(X,Z)
+V = mapping_z(X,Z)
+
+print(f"U shape: {np.shape(U)}")
+r = np.power(np.add(np.power(U,2), np.power(V,2)),0.5)
+skip = (slice(None, None, 8), slice(None, None, 8))
+plt.quiver(X, Z, U/r, V/r, width=.005, pivot='mid')
+plt.savefig("Bx_Bz.png", bbox_inches="tight", dpi=300, )
